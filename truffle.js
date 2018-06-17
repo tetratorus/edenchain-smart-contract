cea18de31d0c249db0371dece4a1ca45b8136b8c
lentan <tetratorus@gmail.com>
2018-06-17 20:58:47 +0800

    Add secrets

--- truffle.js
+++ truffle.js
@@ -1,18 +1,15 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+var HDWalletProvider = require('truffle-hdwallet-provider');
+var secrets = require('./secrets.json');
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(secrets.mnemonic, "https://ropsten.infura.io/" + secrets.infura_key, 2);
+      },
+      network_id: '*',
+      gas: 3500000,
+      gasPrice: 50000000000,
+    }
+  } 
 };
