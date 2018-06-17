4a86d7ac8725ea601e13683c8af9f7126a09717e
lentan <tetratorus@gmail.com>
2018-06-17 23:03:48 +0800

    Add some other config

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,26 +1,24 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.17;
 
 
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
 
-  constructor() public {
-    owner = msg.sender;
+  modifier restricted() {
+    if (msg.sender == owner) _;
   }
 
-  modifier restricted() {
-    if (msg.sender == owner) {
-      _;
-    }
+  constructor() public {
+    owner = msg.sender;
   }
 
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
   }
 
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
+  function upgrade(address newAddress) public restricted {
+    Migrations upgraded = Migrations(newAddress);
     upgraded.setCompleted(last_completed_migration);
   }
 }
--- truffle.js
+++ truffle.js
@@ -1,21 +1,33 @@
-var HDWalletProvider = require('truffle-hdwallet-provider');
-var secrets = require('./secrets.json');
-
-console.log(secrets);
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const secrets = require('./secrets.json');
+const mnemonic = secrets.mnemonic;
+const key = secrets.infura_key;
 
 module.exports = {
   networks: {
-    ropsten: {
-      provider: new HDWalletProvider(secrets.mnemonic, "https://ropsten.infura.io/" + secrets.infura_key, 2),
+   development: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*', // Match any network id
+      gas: 3500000,
+    },
+   ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + key),
       network_id: '*',
       gas: 3500000,
       gasPrice: 50000000000,
-    }
+    },
+    rinkeby: {
+       provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/" + key),
+       network_id: '*',
+       gas: 3500000,
+       gasPrice: 5000000000,
+     },
   },
   solc: {
     optimizer: {
       enabled: true,
       runs: 200,
     },
-  }, 
+  },
 };
