24490eb5d8dc8abf10760091a2bf7c7cc1687da5
lentan <tetratorus@gmail.com>
2018-06-17 22:24:39 +0800

    Modify solhint and migrations

--- truffle.js
+++ truffle.js
@@ -1,15 +1,21 @@
 var HDWalletProvider = require('truffle-hdwallet-provider');
 var secrets = require('./secrets.json');
 
+console.log(secrets);
+
 module.exports = {
   networks: {
     ropsten: {
-      provider: function() {
-        return new HDWalletProvider(secrets.mnemonic, "https://ropsten.infura.io/" + secrets.infura_key, 2);
-      },
+      provider: new HDWalletProvider(secrets.mnemonic, "https://ropsten.infura.io/" + secrets.infura_key, 2),
       network_id: '*',
       gas: 3500000,
       gasPrice: 50000000000,
     }
-  } 
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  }, 
 };
