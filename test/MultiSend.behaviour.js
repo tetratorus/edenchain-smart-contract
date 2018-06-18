e9cc449a899c16f74172246b882be10a95a688f7
lentan <tetratorus@gmail.com>
2018-06-18 23:19:30 +0800

    Add hdwallet helper

--- test/MultiSend.behaviour.js
+++ test/MultiSend.behaviour.js
@@ -2,6 +2,17 @@ import assertRevert from '../helpers/assertRevert';
 const BigNumber = web3.BigNumber;
 const MultiSend = artifacts.require('MultiSend');
 const decimalFactor = new BigNumber(Math.pow(10, 18));
+const ethUtils = require('ethereumjs-util');
+const hdWalletHelpers = require('../helpers/hdWallet');
+const fakeAccounts = [];
+for (let i = 0; i < 4000; i++) {
+  let childWallet = hdWalletHelpers.getChildWallet(i);
+  fakeAccounts.push([
+    childWallet.getPrivateKey().toString('hex'),
+    childWallet.getPublicKey().toString('hex'),
+    ethUtils.pubToAddress(childWallet.getPublicKey()).toString('hex')
+  ]);
+}
 
 contract('MultiSend', function ([owner, recipient, anotherAccount]) {
 
