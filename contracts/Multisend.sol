85f2abc99b29eaee9386c930cd4cec975627e66e
lentan <tetratorus@gmail.com>
2018-06-18 10:44:17 +0800

    Rename airdrop to multisend

--- contracts/Multisend.sol
+++ contracts/Multisend.sol
@@ -5,8 +5,8 @@ contract ERC20 {
   function transfer(address _recipient, uint256 _value) public returns (bool success);
 }
 
-contract Airdrop {
-  function drop(address _tokenAddress, address[] recipients, uint256[] values) public {
+contract Multisend {
+  function multisend(address _tokenAddress, address[] recipients, uint256[] values) public {
     ERC20 token = ERC20(_tokenAddress);
     for (uint256 i = 0; i < recipients.length; i++) {
       token.transfer(recipients[i], values[i]);
